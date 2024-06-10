dab71de512f59cdb117a3313dadfe886a2e1209d
SkyfuryX <121438046+SkyfuryX@users.noreply.github.com>
2024-06-10 16:39:32 -0400

    updated db connection to async

--- src/bot.py
+++ src/bot.py
@@ -1,22 +1,21 @@
 import bot_func as bf
-import os, re, random, math, discord
+import os, re, random, discord, asyncio
 from discord.ext import commands
 from dateutil.relativedelta import relativedelta
 from datetime import datetime as dt, timezone, timedelta, tzinfo
 from dotenv import load_dotenv
-from azure.cosmos import CosmosClient
 
 #examples https://github.com/Rapptz/discord.py/tree/master/examples
 
 load_dotenv()
 token = os.getenv('bot_token')
-db_uri = os.getenv('account_uri')
-db_key = os.getenv('account_key')
+inspiration = open('liberty.txt', 'r').readlines()
+message_time = {}
 
 #initialize db connection
-client = CosmosClient(url=db_uri, credential=db_key)
+'''client = CosmosClient(url=db_uri, credential=db_key)
 database_name = 'democracy_bot'
-database = client.get_database_client(database_name)
+database = client.get_database_client('democracy_bot')'''
 
 #initiate bot
 intents = discord.Intents.default()
@@ -24,17 +23,12 @@ intents.message_content = True
 descrip = '''A discord bot to view on-demand statistics from the game Helldrivers 2: Includes planet info, 
 Major Orders, dispatches from Super Earth, and more.'''
 bot = commands.Bot(command_prefix='!', description=descrip, intents=intents)
-tree = discord.app_commands.CommandTree
-
-inspiration = open('liberty.txt', 'r').readlines()
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     await bot.change_presence(activity=discord.CustomActivity(name='Spreading Managed Democracy'))
     print('Logged on as', bot.user)
-
-message_time = {}
 
 @bot.listen('on_message')
 async def on_message(message):
@@ -49,62 +43,60 @@ async def on_message(message):
             message_time[message.guild.id] = dt.now()
             print(message_time[message.guild.id])
             i = random.randint(1,len(inspiration))
-            print(i)
             await message.channel.send(inspiration[i-1][0:-1])
         elif (message.guild.id in message_time.keys()) == True:
             if (dt.now() - message_time[message.guild.id]).total_seconds() > 60:
                 message_time[message.guild.id] = dt.now()
                 i = random.randint(1,len(inspiration))
                 await message.channel.send(inspiration[i-1][0:-1])
-  
         
 @bot.tree.command(name='war',description='Show the stats of the current Galactic War') #shows info on the galatic war in general
 async def war(interaction: discord.Interaction):
-    msg = bf.war()    
-    await interaction.channel.send(msg)
+    msg = await bf.war()   
+    await interaction.response.send_message(msg)
 
 @bot.tree.command(name='orders',description='Returns the current Major Order') #shows info on the current Major Order in general
 async def orders(interaction: discord.Interaction):
-    msg = bf.orders() 
-    await interaction.channel.send(msg)
+    msg = await bf.orders()
+    await interaction.response.send_message(msg)
 
 @bot.tree.command(name='dispatch', description='Returns most recent dispatch message from Super Earth.')
 async def dispatch(interaction: discord.Interaction, number: int = 1):
     #displays latest dispatch messages
-    container = database.get_container_client('dispatch')
+    query='SELECT d.published, d.message FROM dispatch d ORDER BY d.id DESC OFFSET 0 LIMIT 1'
+    results = await bf.db_query('dispatch', query)
     msg = 'Latest message(s) from Super Earth:'
-    for item in container.query_items(
-        query='SELECT d.published, d.message FROM dispatch d ORDER BY d.id DESC OFFSET 0 LIMIT 1', 
-        enable_cross_partition_query=True):
+    for item in results:
         msg += '\n\nDate: ' + str(dt.strptime(item['published'][0:10], '%Y-%m-%d') + relativedelta(years=160))[0:10] + '\n' + item['message'] #adds 160 years to date to make message more thematic
     if number <= 3:
-        await interaction.channel.send(msg)
+        await interaction.response.send_message(msg)
     elif number >= 4:
+        await interaction.response.send_message('-Dispatches sent via Democratic Direct Message-')
         await interaction.user.send(msg)
                    
 @bot.tree.command(name='planets',description='Displays information on a specific planet')#gathers liberation info and stats on a specific planet
 async def planets(interaction: discord.Interaction, name: str):
-    msg = bf.planet(name)
-    await interaction.channel.send(msg)
+    msg = await bf.planet(name)
+    await interaction.response.send_message(msg)
     
 @bot.tree.command(name='campaigns',description='Show the current active Libration and Defense campaigns.') 
 async def campaigns(interaction: discord.Interaction): 
-    msg = bf.campaigns()  
-    await interaction.channel.send(msg)
+    msg = await bf.campaigns()
+    await interaction.response.send_message(msg)
 
 @bot.tree.command(name='weapons',description='In Progress') #show information on weapons
 async def weapons(interaction: discord.Interaction,weapon: str):
     msg = 'Work in progress'  
-    await interaction.channel.send(msg)
+    await interaction.response.send_message(msg)
 
 @bot.tree.command(name='stratgems',description='In Progress') #show info on requested stratgems
 async def strategems(interaction: discord.Interaction,strat: str):
     msg = 'Work in progress'  
-    await interaction.channel.send(msg)
+    await interaction.response.send_message(msg)
     
 @bot.command()
 async def shutdown(ctx):
     if ctx.author.id == 157695574580264960: 
-        await bot.close()
-
+        await bot.close()    
+    
 bot.run(token) #starts bot and begins listening for events and commands
--- src/bot_func.py
+++ src/bot_func.py
@@ -4,22 +4,26 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime as dt, timezone, timedelta
 from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
-from azure.cosmos import CosmosClient
+#from azure.cosmos import CosmosClient
+from azure.cosmos.aio import CosmosClient
 
 load_dotenv()
-token = os.getenv('bot_token')
 db_uri = os.getenv('account_uri')
 db_key = os.getenv('account_key')
 
-client = CosmosClient(url=db_uri, credential=db_key)
-database_name = 'democracy_bot'
-database = client.get_database_client(database_name)
+async def db_query(cont_name, db_query):
+    async with CosmosClient(url=db_uri, credential=db_key) as client:
+        database =  client.get_database_client('democracy_bot')
+        container = database.get_container_client(cont_name)
+        #results =  await container.query_items(query=db_query)
+        results = [item async for item in container.query_items(query=db_query)]
+        return results
 
-def commas(number):
+async def commas(number):
     numlst = list(str(number))
     x = int(math.floor(len(numlst) / 3))
     i = 1
-    if len(numlst) <=1:
+    if len(numlst) <=3:
        pass #do nothing since it needs no commas added 
     elif len(numlst) % 3 == 0:
         for n in range(x-1):
@@ -32,22 +36,20 @@ def commas(number):
     numlst = ''.join(numlst)
     return numlst #return string to be concat'd into messages
 
-def war():
-    container = database.get_container_client('war_status')
-    for item in container.query_items(
-        query='SELECT * FROM war_status w ORDER BY w.id DESC OFFSET 0 LIMIT 1',
-        enable_cross_partition_query=True):
+async def war():
+    query='SELECT * FROM war_status w ORDER BY w.id DESC OFFSET 0 LIMIT 1'
+    results = await db_query('war_status', query)
+    for item in results:
         war = item 
-    msg = ('**--Galactic War Stats--**\nHelldivers Active: '+ commas(war['playerCount']) +'\nHelldivers KIA: ' + commas(war['deaths']) + 
-           '\nAutomatons Killed: '+ commas(war['automatonKills']) +'\nTerminids Killed: '+ commas(war['terminidKills']) +
-           '\nIlluminate Killed: '+ commas(war['illuminateKills']) +'\nBullets Fired: ' + commas(war['bulletsFired']))
+    msg = ('**--Galactic War Stats--**\nHelldivers Active: '+ await commas(war['playerCount']) +'\nHelldivers KIA: ' + await commas(war['deaths']) + 
+           '\nAutomatons Killed: '+ await commas(war['automatonKills']) +'\nTerminids Killed: '+ await commas(war['terminidKills']) +
+           '\nIlluminate Killed: '+ await commas(war['illuminateKills']) +'\nBullets Fired: ' + await commas(war['bulletsFired']))
     return msg
 
-def orders():
-    container = database.get_container_client('major_orders')
-    for item in container.query_items(
-        query='SELECT o.title, o.briefing, o.description, o.expiration, o.tasks, o.progress FROM major_orders o ORDER BY o.id DESC OFFSET 0 LIMIT 1',
-        enable_cross_partition_query=True):
+async def orders():
+    query='SELECT o.title, o.briefing, o.description, o.expiration, o.tasks, o.progress FROM major_orders o ORDER BY o.id DESC OFFSET 0 LIMIT 1'
+    results = await db_query('major_orders', query)
+    for item in results:
         order = item
     msg = ('**--' + order['title'] + '--**\n' + order['briefing'] + '\n'+order['description'])
     now = dt.now(timezone.utc) + timedelta(hours=4)
@@ -59,9 +61,9 @@ def orders():
         if task['type'] == 11:
             planetIDs.append(str(task['values'][2]))
     if len(planetIDs) > 0:
-        container = database.get_container_client('planets')
-        for item in container.query_items(query='SELECT p.name, p.currentOwner, p.maxHealth, p.health FROM planets p WHERE p.index IN ('+', '.join(planetIDs)+')',
-            enable_cross_partition_query=True):
+        query = 'SELECT p.name, p.currentOwner, p.maxHealth, p.health FROM planets p WHERE p.index IN ('+', '.join(planetIDs)+')'
+        results = await db_query('planets', query)
+        for item in results:
             if item['currentOwner'] == 'Humans' and (item['health']/item['maxHealth']) == 1:
                 msg += '\n> '+ item['name'] + ' - 100% Liberated'
             else:
@@ -69,10 +71,10 @@ def orders():
         msg += ('\nTime Remaining: ' + str(timeleft.days) + ' days ' + str(hoursleft) + ' hours ' + str(minsleft) + ' minutes')
         return msg
     
-def planet(name):
-    container = database.get_container_client('planets')    
-    for item in container.query_items(query='SELECT * FROM planets p WHERE p.name = "'+name.upper()+'"',
-            enable_cross_partition_query=True): 
+async def planet(name):
+    query='SELECT * FROM planets p WHERE p.name = "'+name.upper()+'"'
+    results = await db_query('planets', query)  
+    for item in results: 
         if item['currentOwner'] == 'Humans':
             item['currentOwner'] = 'Super Earth'
         stats = item['statistics']
@@ -85,22 +87,21 @@ def planet(name):
             msg += '\n'+str(abs(round((event['health']/event['maxHealth'] -1)*100, 4))) + '% Defended'
         else:
             msg += '\n'+str(abs(round((item['health']/item['maxHealth'] -1)*100, 4))) + '% Liberated'
-        msg += ('\n------------------\nHelldivers Active: '+ commas(stats['playerCount']) +'\nEnemies Killed: '+commas(stats['enemiesKilled'])+
-                '\nHelldivers KIA: ' + commas(stats['deaths']) + '\nBullets Fired: '+commas(stats['bulletsFired']))
+        msg += ('\n------------------\nHelldivers Active: '+ await commas(stats['playerCount']) +'\nEnemies Killed: '+ await commas(stats['enemiesKilled'])+
+                '\nHelldivers KIA: ' + await commas(stats['deaths']) + '\nBullets Fired: '+ await commas(stats['bulletsFired']))
         return msg
     msg = '-Planet not found-'
     return msg
             
-  
-def campaigns():      
-    container = database.get_container_client('campaigns')
+async def campaigns():      
     msg = '**--Active Campaigns--**'
     defense = '\n-Defense Campaigns- \n|'
     autofrnt = '\n-Automaton Front-\n|'
     bugfrnt = '\n-Terminid Front-\n|'
-    illufrnt = '\n-Illuminate Front-\n|' # in preparation for Illuminate to be in-game
-    for item in container.query_items(query='SELECT * FROM campaigns c',
-            enable_cross_partition_query=True):
+    illufrnt = '\n-Illuminate Front-\n|' #in preparation for Illuminate to be in game
+    query='SELECT * FROM campaigns c'
+    results = await db_query('campaigns', query)
+    for item in results:
         if item['planet']['currentOwner'] == 'Humans' and item['planet']['event'] == True:
             defense += ' ' + item['planet']['name'] + ' ' + str(abs(round((item['planet']['event']['health']/item['planet']['event']['maxHealth'] -1)*100, 4))) + '% |'
         elif item['planet']['currentOwner'] == 'Automaton':
