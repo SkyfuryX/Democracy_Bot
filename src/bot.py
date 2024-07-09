b50b1aab1dd63b50f029859dccd628d795d48108
SkyfuryX <121438046+SkyfuryX@users.noreply.github.com>
2024-07-09 17:39:55 -0400

    Updated responses into embeds

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,4 @@
-import bot_func as bf
+import test_func as tf
 import os, re, random, discord, asyncio
 from discord import app_commands
 from discord.ext import commands
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 #examples https://github.com/Rapptz/discord.py/tree/master/examples
 
 load_dotenv()
-token = os.getenv('bot_token')
+token = os.getenv('bot_test_token')
 inspiration = open('liberty.txt', 'r').readlines()
 message_time = {}
 settings = {}
@@ -71,70 +71,80 @@ async def on_message(message):
 @bot.tree.command(name='war',description='Display the overall stats of the current Galactic War') #shows info on the galatic war in general
 @app_commands.describe(public='Select True to share the response in this channel.')
 async def war(interaction: discord.Interaction, public: bool=False):
-    msg = await bf.war()   
-    await interaction.response.send_message(content=msg, ephemeral=not public)
+    msg = await tf.war()   
+    await interaction.response.send_message(embed=msg, ephemeral=not public)
 
 @bot.tree.command(name='orders', description='Displays the current Major Order') #shows info on the current Major Order in general
 @app_commands.describe(public='Select True to share the response in this channel.')
 async def orders(interaction: discord.Interaction, public: bool=False):
-    msg = await bf.orders()
-    await interaction.response.send_message(content=msg, ephemeral=not public)
+    msg = await tf.orders()
+    await interaction.response.send_message(embed=msg, ephemeral=not public)
 
-@bot.tree.command(name='dispatch', description='Displays the [number] most recent dispatch message(s) from Super Earth. Defaults to 1, max 3')
-@app_commands.describe(number='The number of messages requested. Max 3', public='Select True to share the response in this channel.')
+@bot.tree.command(name='dispatch', description='Displays the [number] most recent dispatch message(s) from Super Earth. Defaults to 1')
+@app_commands.describe(number='The number of messages requested.', public='Select True to share the response in this channel.')
 async def dispatch(interaction: discord.Interaction, number: int=1, public: bool=False):
     #displays latest dispatch messages
     query='SELECT d.published, d.message FROM dispatch d ORDER BY d.id DESC OFFSET 0 LIMIT '+ str(number)
-    results = await bf.db_query('dispatch', query)
-    msg = 'Latest message(s) from Super Earth:'
+    results = await tf.db_query('dispatch', query)
+    msg = discord.Embed(title='**--Latest Message(s) from Super Earth--**')
     for item in results:
-        msg += '\n\nDate: ' + str(dt.strptime(item['published'][0:10], '%Y-%m-%d') + relativedelta(years=160))[0:10] + '\n' + item['message'] #adds 160 years to date to make message more thematic
-    if number <= 3:
-        await interaction.response.send_message(content=msg, ephemeral=not public)
-    elif number >= 4:
-        await interaction.response.send_message(content='Super Earth cannot grant your information request at this time. Request 3 or less dispatches.', ephemeral=True)
+        msg.add_field(name='Date: ' + str(dt.strptime(item['published'][0:10], '%Y-%m-%d') + relativedelta(years=160))[0:10], value= item['message'], inline=False) #adds 160 years to date to make message more thematic
+    if len(msg) <= 6000:
+        await interaction.response.send_message(embed=msg, ephemeral=not public)
+    elif len(msg) > 6000:
+        msg = discord.Embed(title='',desctiption='Super Earth cannot grant your information request at this time. Request fewer dispatches.')
+        await interaction.response.send_message(embed= msg, ephemeral=True)
                            
 @bot.tree.command(name='planets',description='Displays information on a specific planet') #gathers liberation info and stats on a specific planet
 @app_commands.describe(name='The name of the planet to view.', public='Select True to share the response in this channel.')
 async def planets(interaction: discord.Interaction, name: str, public: bool=False):
-    msg = await bf.planet(name)
-    await interaction.response.send_message(content=msg,ephemeral= not public)
+    msg = await tf.planet(name)
+    await interaction.response.send_message(embed=msg,ephemeral= not public)
     
 @planets.autocomplete('name')
 async def planets_autocomplete(
     interaction: discord.Interaction,
     current: str
 ) -> list[app_commands.Choice[str]]:
-    return [
-        app_commands.Choice(name=planet, value=planet)
-        for planet in planetlist if current.upper() in planet]
+    try:
+        return [
+            app_commands.Choice(name=planet, value=planet)
+            for planet in planetlist if current.upper() in planet]
+    except discord.errors.HTTPException:
+        print('Too Many Autocomplete Results')
         
 @bot.tree.command(name='campaigns',description='Displays the currently available Liberation and Defense campaigns.') 
 @app_commands.describe(public='Select True to share the response in this channel.')
 async def campaigns(interaction: discord.Interaction, public: bool=False): 
-    msg = await bf.campaigns()
-    await interaction.response.send_message(content=msg,ephemeral= not public)
-
-'''@bot.tree.command(name='weapons',description='In Progress') #show information on weapons
-async def weapons(interaction: discord.Interaction,weapon: str):
-    msg = 'Work in progress'  
-    await interaction.response.send_message(content=msg)'''
+    libcam, defcam = await tf.campaigns()
+    if len(defcam) > 25:
+        await interaction.response.send_message(embeds=[libcam,defcam], ephemeral= not public)
+    else:
+        await interaction.response.send_message(embed=libcam, ephemeral= not public)
 
 @bot.tree.command(name='stratagems',description='Displays information for any stratagems containing the name included') #show info on requested stratgems
 @app_commands.describe(name='The name of the stratagem to view.', public='Select True to share the response in channel.')
 async def stratagems(interaction: discord.Interaction, name: str, public: bool=False):
-    msg = await bf.stratagems(name)
-    await interaction.response.send_message(content=msg, ephemeral= not public)
+    msg = await tf.stratagems(name)
+    await interaction.response.send_message(embed=msg, ephemeral= not public,)
     
 @stratagems.autocomplete('name')
 async def stratagems_autocomplete(
     interaction: discord.Interaction,
     current: str
 ) -> list[app_commands.Choice[str]]:
-    return [
-        app_commands.Choice(name=stratagem, value=stratagem)
-        for stratagem in stratlist if current.lower() in stratagem.lower()]
-    
+    try:
+        return [
+            app_commands.Choice(name=stratagem, value=stratagem)
+            for stratagem in stratlist if current.lower() in stratagem.lower()]
+    except discord.errors.HTTPException:
+       pass
+ 
+@bot.tree.command(name='report', description='Issues the F-33D form link to provide feedback to Super Earth High Command')  
+async def report(interaction: discord.Interaction):
+    msg = discord.Embed(title='--Reports to Super Earth--', description= 'Please submit bug reports and feature requests via form [F-33D](https://forms.gle/mAPt9wcj4qeaT2g26)\n\nThank you for your service, Helldiver!', type='rich')
+    await interaction.response.send_message(embed= msg,ephemeral=True)    
+          
 @bot.command()
 async def shutdown(ctx):
     if ctx.author.id == 157695574580264960: 
@@ -147,21 +157,6 @@ async def sync(ctx):
         await ctx.channel.send(content='Commands Synchronized') 
 
 async def guild_id(interaction: discord.Interaction):
-    return interaction.guild_id
-
-@bot.tree.command(name='settings', description='Configuration settings')
-@commands.check_any(commands.has_permissions(administrator=True))
-async def settings(interaction: discord.Interaction, public: bool, inspiration_freq: int, role:str):
-    guild_set = {'id':str(interaction.guild_id)}
-    guild_set['settings'] =  {'default_privacy': public,'inspiration_freq': inspiration_freq, 'role': role}
-    container = await bf.data_upload('settings')
-    #settings[interaction.guild_id] = guild_set['settings']
-    await container.upsert_item(guild_set)
-    await interaction.response.send_message(content='-Settings Saved-\nPrivacy: '+ str(guild_set['settings']['privacy'])+'\nInspiration Freq: '+
-                                            str(guild_set['settings']['privacy'])+ 's\nMinimum Role: '+ guild_set['settings']['privacy'], ephemeral=True)
-    
-@bot.tree.command(name='report', description='Issues the F-33D form link to provide feedback to Super Earth High Command')  
-async def report(interaction: discord.Interaction):
-    await interaction.response.send_message(content = 'Please submit bug reports and feature requests via form [F-33D](https://forms.gle/mAPt9wcj4qeaT2g26) \n\nThank you for your service, Helldiver!',ephemeral=True)    
-       
+    return interaction.guild_id    
+     
 bot.run(token) #starts bot and begins listening for events and commands
--- src/bot_func.py
+++ src/bot_func.py
@@ -41,13 +41,21 @@ async def commas(number):
     return numlst #return string to be concat'd into messages
 
 async def war():
-    query='SELECT * FROM war_status w ORDER BY w.id DESC OFFSET 0 LIMIT 1'
+    query='SELECT * FROM war_status w ORDER BY w._ts DESC OFFSET 0 LIMIT 1'
     results = await db_query('war_status', query)
-    for item in results:
-        war = item 
-    msg = ('**--Galactic War Stats--**\nHelldivers Active: '+ await commas(war['playerCount']) +'\nHelldivers KIA: ' + await commas(war['deaths']) + 
-           '\nAutomatons Killed: '+ await commas(war['automatonKills']) +'\nTerminids Killed: '+ await commas(war['terminidKills']) +
-           '\nIlluminate Killed: '+ await commas(war['illuminateKills']) +'\nBullets Fired: ' + await commas(war['bulletsFired']))
+    war = results[0]
+    msg = discord.Embed(title='**--Galactic War--**', type='rich')
+    msg.add_field(name='Helldivers Active:', value=await commas(war['playerCount']))
+    msg.add_field(name='Successful Missions:', value=await commas(war['missionsWon']))
+    msg.add_field(name='Failed Missions:', value=await commas(war['missionsLost']))
+    msg.add_field(name='Bullets Fired:', value=await commas(war['bulletsFired']))
+    msg.add_field(name='Helldivers KIA:', value=await commas(war['deaths']), )
+    msg.add_field(name='Accidentals:', value=await commas(war['friendlies']))
+    msg.add_field(name='', value='', inline=False)
+    msg.add_field(name='-Enemies Liberated-', value='',inline=False)
+    msg.add_field(name='Automatons:', value=await commas(war['automatonKills']))
+    msg.add_field(name='Terminids:', value=await commas(war['terminidKills']))
+    msg.add_field(name='Illuminate:', value=await commas(war['illuminateKills']))
     return msg
 
 async def orders():
@@ -55,27 +63,34 @@ async def orders():
     results = await db_query('major_orders', query)
     for item in results:
         order = item
-    msg = ('**--' + order['title'] + '--**\n' + order['briefing'] + '\n'+order['description'])
     now = dt.now(timezone.utc) + timedelta(hours=4)
     timeleft = dt.strptime(order['expiration'], '%Y-%m-%d %H:%M:%S').astimezone(timezone.utc) - now #find time remaining for objective
     if timeleft.days < 0:
-        msg = '-Awaiting Orders from Super Earth-'
+        msg = discord.Embed(title='-Awaiting Orders from Super Earth-', type='rich')
         return msg
     hoursleft = math.floor(timeleft.seconds/3600)
     minsleft =  math.floor((timeleft.seconds/3600-hoursleft)*60)
+    msg = discord.Embed(title='**--' + order['title'] + '--**', type='rich')
+    msg.add_field(name=order['briefing'], value= order['description'], inline = False)
     planetIDs = []
     for task in order['tasks']: #Determine objective type
         if task['type'] == 11 or task['type'] == 13:
             planetIDs.append(str(task['values'][2]))
     if len(planetIDs) > 0:
+        i=0
         query = 'SELECT p.name, p.currentOwner, p.maxHealth, p.health FROM planets p WHERE p.index IN ('+', '.join(planetIDs)+')'
         results = await db_query('planets', query)
         for item in results:
             if item['currentOwner'] == 'Humans' and (item['health']/item['maxHealth']) == 1:
-                msg += '\n> '+ item['name'] + ' - 100% Liberated'
+                i +=1
+                msg.add_field(name= item['name'], value= '100% Liberated', inline=True)
             else:
-                msg += '\n> '+ item['name'] + ' - ' + str(abs(round((item['health']/item['maxHealth'] -1)*100, 4))) + '% Liberated'
-        msg += ('\nTime Remaining: ' + str(timeleft.days) + ' days ' + str(hoursleft) + ' hours ' + str(minsleft) + ' minutes')
+                i +=1
+                msg.add_field(name= item['name'], value= str(abs(round((item['health']/item['maxHealth'] -1)*100, 4))) + '% Liberated', inline= True)
+        if i % 3 != 0:
+            for x in range(int(round(i/3,0)), 3):
+                msg.add_field(name='', value='')
+        msg.add_field(name='Time Remaining', value= str(timeleft.days) + ' days ' + str(hoursleft) + ' hours ' + str(minsleft) + ' minutes', inline=False)
     return msg
     
 async def planet(name):
@@ -86,76 +101,102 @@ async def planet(name):
             item['currentOwner'] = 'Super Earth'
         stats = item['statistics']
         stats['enemiesKilled'] = stats['illuminateKills']+stats['automatonKills']+stats['terminidKills']
-        msg = '**--'+item['name']+'--** \n'+item['currentOwner']+' Control'
+        #msg = discord.Embed(title='**--'+item['name']+'--**', description= item['currentOwner']+' Control', type='rich')
+        #msg = '**--'+item['name']+'--** \n'+item['currentOwner']+' Control'
         if item['currentOwner'] == 'Super Earth' and (item['health']/item['maxHealth']) == 1 and item['event'] == None:
-            msg += '\n100% Liberated'
+            msg = discord.Embed(title='**--'+item['name']+'--**', description= item['currentOwner']+' Control\n100% Liberated', type='rich')
         elif item['currentOwner'] == 'Super Earth' and (item['health']/item['maxHealth']) == 1 and item['event'] == True:
             event = item['event']
-            msg += '\n'+str(abs(round((event['health']/event['maxHealth'] -1)*100, 4))) + '% Defended'
+            msg = discord.Embed(title='**--'+item['name']+'--**', description= item['currentOwner']+' Control\n'+str(abs(round((event['health']/event['maxHealth'] -1)*100, 4))) + '% Defended', type='rich')
         else:
-            msg += '\n'+str(abs(round((item['health']/item['maxHealth'] -1)*100, 4))) + '% Liberated'
-        msg += '\nSector: '+ item['sector'] +'\nBiome and Hazards: ' + item['biome']['name'] + ' - '
+            msg = discord.Embed(title='**--'+item['name']+'--**', description= item['currentOwner']+' Control\n'+str(abs(round((item['health']/item['maxHealth'] -1)*100, 4))) + '% Liberated', type='rich')
+        msg.add_field(name='Sector:', value=item['sector'])
         if len(item['hazards']) > 1:
-            msg += item['hazards'][0]['name'] + ', '+ item['hazards'][1]['name']
+            msg.add_field(name='Biome and Hazards:', value=item['biome']['name']+' - '+ item['hazards'][0]['name'] + ', '+ item['hazards'][1]['name'])
         else:
-            msg += item['hazards'][0]['name']
+            msg.add_field(name='Biome and Hazards:', value=item['biome']['name']+' - '+ item['hazards'][0]['name'])
         query2='SELECT p.name FROM planets p WHERE p.id IN ("'+ '","'.join(str(x) for x in item['waypoints'])  +'")'
         planetlst = await db_query('planets', query2)
         if len(planetlst) > 0:
-            msg += '\nSupply Lines: ' + ', '.join(x['name'] for x in planetlst)
-        msg += ('\n------------------\nHelldivers Active: '+ await commas(stats['playerCount']) +'\nEnemies Killed: '+ await commas(stats['enemiesKilled'])+
-                '\nHelldivers KIA: ' + await commas(stats['deaths']) + '\nBullets Fired: '+ await commas(stats['bulletsFired']))
+            msg.add_field(name='Supply Lines:', value=', '.join(x['name'] for x in planetlst)) 
+        else:
+            msg.add_field(name ='', value='')
+        msg.add_field(name='----------------------------------', value='', inline= False)
+        msg.add_field(name='Helldivers Active:', value=await commas(stats['playerCount']))
+        msg.add_field(name='Bullets Fired:', value=await commas(stats['bulletsFired']))
+        msg.add_field(name='', value='')
+        msg.add_field(name='Helldivers KIA:', value=await commas(stats['deaths']), )
+        msg.add_field(name='Enemies Liberated:', value=await commas(stats['enemiesKilled']))
+        msg.add_field(name='', value='')
         return msg
-    msg = '-Planet not found-'
+    msg = discord.Embed(title='-Planet Not Found-', type='rich')
     return msg
             
 async def campaigns():      
-    msg = '**--Active Campaigns--**'
-    defense = '\n-Defense Campaigns- \n|'
-    autofrnt = '\n-Automaton Front-\n|'
-    bugfrnt = '\n-Terminid Front-\n|'
-    illufrnt = '\n-Illuminate Front-\n|' #in preparation for Illuminate to be in game
+    autodef = []
+    bugdef = []
+    illudef= []
+    autolib = []
+    buglib = []
+    illulib = [] #in preparation for Illuminate to be in game
+    libcam = discord.Embed(title='**--Liberation Campaigns--**', type='rich')
+    defcam = discord.Embed(title='**--Defense Campaigns--**', type='rich')
     query='SELECT * FROM campaigns c'
     results = await db_query('campaigns', query)
     for item in results:
         if item['planet']['currentOwner'] == 'Humans' and len(item['planet']['event']) > 0:
-            defense += ' ' + item['planet']['name'] + ' ' + str(abs(round((item['planet']['event']['health']/item['planet']['event']['maxHealth'] -1)*100, 4))) + '% |'
+            if item['planet']['event']['faction'] == 'Terminids':
+                bugdef.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['event']['health']/item['planet']['event']['maxHealth'] -1)*100, 4))) + '%')
+            if item['planet']['event']['faction'] == 'Automaton':
+                autodef.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['event']['health']/item['planet']['event']['maxHealth'] -1)*100, 4))) + '%')
+            if item['planet']['event']['faction'] == 'Illuminate':
+                illudef.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['event']['health']/item['planet']['event']['maxHealth'] -1)*100, 4))) + '%')
         elif item['planet']['currentOwner'] == 'Automaton':
-            autofrnt += ' '+ item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '% |'
+            autolib.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '%')
         elif item['planet']['currentOwner'] == 'Terminids':
-            bugfrnt += ' '+ item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '% |'
+            buglib.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '%')
         elif item['planet']['currentOwner'] == 'Illuminate':
-            illufrnt += ' '+ item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '% |'
+            illulib.append(item['planet']['name'] + ' ' + str(abs(round((item['planet']['health']/item['planet']['maxHealth'] -1)*100, 4))) + '%')
     #only add each section is it has extra content
-    if len(autofrnt) > 20:
-        msg += autofrnt
-    if len(bugfrnt) > 19:
-        msg += bugfrnt    
-    if len(illufrnt) > 21:
-        msg += illufrnt    
-    if len(defense) > 23:
-        msg += defense
-    return msg
+    if len(autolib) > 0:
+        libcam.add_field(name ='Automaton Front:', value = ', '.join(autolib),inline=False) 
+    if len(buglib) > 0:
+        libcam.add_field(name ='Terminid Front:', value = ', '.join(buglib),inline=False)   
+    if len(illulib) > 0:
+        libcam.add_field(name ='Illuminate Front:', value = ', '.join(illulib), inline=False)     
+    if len(autodef) > 0:
+        defcam.add_field(name ='Automaton Front:', value = ', '.join(autodef), inline=False) 
+    if len(bugdef) > 0:
+        defcam.add_field(name ='Terminid Front:', value = ', '.join(bugdef), inline=False)
+    if len(illudef) > 0:
+        defcam.add_field(name ='Illuminate Front:', value = ', '.join(illudef), inline=False)
+    return libcam, defcam
+
             
 async def stratagems(name):
     emojikeys = {'up': ':arrow_up:','down':':arrow_down:','left':':arrow_left:','right':':arrow_right:'}
     query = 'SELECT * FROM stratagems s WHERE CONTAINS(s.name, "'+name+'", true) OFFSET 0 LIMIT 1'
     results = await db_query('stratagems', query)
-    if results == None:
-        msg = '-Stratagem Not Found-'
+    if len(results) == 0:
+        msg = discord.Embed(title='-Stratagem Not Found-', type='rich')
         return msg
     else:
-        msg = '**--Stratagem Info--**\n'
         for item in results:
             if item['codename'] != None:
-                msg += item['codename'] + ' - ' + item['name']
+                msg = discord.Embed(title='**--'+item['codename'] + ' - ' + item['name']+'--**', type='rich')
             else:
-                msg += item['name']
+                msg = discord.Embed(title='**--'+item['name']+'--**', type='rich')
             if item['activation'] == None:
                 item['activation'] = 0
             if item['cooldown'] == None:
                 item['cooldown'] = 0      
-            msg += '\n> Call-in Time: ' + str(item['activation']) + ' sec\n> Uses: ' + item['uses'] + '\n> Cooldown Time: '+ str(item['cooldown']) + ' sec\n> Keys: '
+            keys = ''
             for key in item['keys']:
-                msg += emojikeys[key]
+                keys += emojikeys[key]
+            msg.add_field(name='Call-in Time:', value= str(item['activation']) + ' sec')
+            msg.add_field(name='Cooldown Time:', value= str(item['cooldown']) + ' sec')
+            msg.add_field(name='', value = '')
+            msg.add_field(name='Uses:', value= item['uses'])
+            msg.add_field(name='Activation:', value= keys)
+            msg.add_field(name='', value = '')
         return msg
