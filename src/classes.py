b35ef540cd2bea0110299998ff5d32e8c4772755
SkyfuryX <gannonshanley@gmail.com>
2026-06-05 13:09:54 -0400

    classes updated to use pydantic for json validation/parsing

--- src/classes.py
+++ src/classes.py
@@ -1,69 +1,113 @@
-class Location:
-    def __init__(self, name, health, maxHealth):
-        self.name = name
-        self.health = health
-        self.maxHealth = maxHealth
-        
-    async def currentHealth(self):
-        return round(self.health / self.maxHealth, 4)
-    
-class Stats:
-    def __init__(self, success, failed, terminid, automaton, illuminate, fired, hit, deaths, accidentals, players):
-        self.success = success
-        self.failed = failed
-        self.terminid = terminid
-        self.automaton = automaton
-        self.illuminate = illuminate
-        self.fired = fired 
-        self.hit = hit
-        self.deaths = deaths
-        self.accidentals = accidentals
-        self.players = players
-        
-    def __repr__(self):
-        return f"Stats({self.success}, {self.failed}, {self.terminid}, {self.automaton}, {self.illuminate}, {self.fired}, {self.hit}, {self.deaths}, {self.accidentals}, {self.players})"
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+ 
+class HelldiversBase(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+    ) 
+
+#General Statistics   
+class Stats(HelldiversBase):
+    missions_won: int
+    missions_lost: int
+    mission_time: int
+    terminid_kills: int
+    automaton_kills: int
+    illuminate_kills: int
+    bullets_fired:int
+    bullets_hit: int
+    timePlayed: int
+    deaths: int
+    revives: int
+    friendlies: int
+    mission_success_rate: int
+    accuracy: int
+    player_count: int 
         
     async def killsCombined(self):
-        return self.terminid + self.automaton + self.illuminate
+        return self.terminid_kills + self.automaton_kills + self.illuminate_kills
+
+#Planet Classes    
+class Biome(HelldiversBase):
+    name: str
+    description: str
+
+class Location(HelldiversBase):
+    health: int
+    max_health: int
+        
+    async def currentHealth(self):
+        return round(self.health / self.max_health, 4)
+
+class Event(Location):
+    event_type: int
+    faction: str
+    start_time: str
+    end_time: str
+    campaign_id: int
+    joint_operation_ids: Optional[list[int]] = None
+
+
+class Position(HelldiversBase):
+    x: float
+    y: float
+
+class Region(HelldiversBase):
+    id: int
+    hash: int
+    name:str
+    description: Optional[str] = None
+    size: str
+    regen_per_second: float
+    availability_factor: float
+    is_available: bool
+    players: int
     
-class Region(Location):
-    async def __init__(self, name, description, health, maxHealth, size, players):
-        super().__init__(name, health, maxHealth)
-        self.description = description
-        self.size = size
-        self.players = players
-        
-    def __repr__(self):
-        return f"Region({self.name}, {self.description}, {self.health}, {self.maxHealth}, {self.size}, {self.players})"
-        
+    def __str__(self):
+        return f"Region({self.name})"
+
 class Planet(Location):
-    async def __init__(self, name, sector, biome, hazards, waypoints, health, maxHealth, owner, stats, regions):
-        super().__init__(name, health, maxHealth)
-        self.sector = sector
-        self.biome = biome
-        self.hazards = hazards
-        self.waypoints = waypoints
-        self.owner = owner
-        self.stats = await self.addStats(stats)
-        self.regions = await self.createRegions(regions)
+    index: int
+    name:str
+    sector: str
+    biome: Biome
+    hazards: list[Biome]
+    hash: int
+    position: Position
+    waypoints: list[int]
+    disabled: bool
+    initial_owner: str
+    current_owner: str
+    regen_per_second: float
+    event: Optional[Event] = None
+    statistics: Stats
+    attacking: list[int]
+    regions:  list[Region]
+    id: str
         
     def __str__(self):
         return f"Planet({self.name})"
     
-    def __repr__(self):
-        return f"Planet({self.name}, {self.sector}, {self.biome}, {self.hazards}, {self.waypoints}, {self.health}, {self.maxHealth}, {self.owner}, {self.regions}, {self.stats})"
-        
-    async def createRegions(regions):
-        return [Region(region["name"], region['description'], region['health'], region["maxHealth"], region["size"],region["players"]) for region in regions]
-   
-    async def addStats(stats):
-        return Stats(stats["missionsWon"],
-                     stats["missionsLost"],
-                     stats["terminidKills"],
-                     stats["automatonKills"],
-                     stats["illuminateKills"],
-                     stats["bulletsFired"],
-                     stats["bulletsHit"],
-                     stats["deaths"],
-                     stats["friendlies"],
-                     stats["playerCount"])
+#Order classes   
+class Reward(HelldiversBase):
+    type: int
+    amount: int
+
+class Task(HelldiversBase):
+    type: int
+    values: list[int]
+    value_types: list[int]
+
+class Order(HelldiversBase):
+    id: int
+    progress: list[int]
+    title: str
+    briefing: str
+    description: Optional[str] = None
+    tasks:  list[Task]
+    reward: Reward
+    rewards: list[Reward]
+    expiration: str
+    flags: int
